f9364df442e37e962cca21aa5cdca3e1ab13ca97
iambharatchavhan <bharatchavhan141@gmail.com>
2024-02-08 21:49:08 +0530

    app updated

--- mentorIntern/src/App.jsx
+++ mentorIntern/src/App.jsx
@@ -12,7 +12,13 @@ import MyLifeComponent from './componentDay2/classComps/LifeCycleMethod'
 import Day3Demo from './day3/class/day3'
 import LifeCycle3 from './day3/class/LifeCycle3'
 import Parent from './day3/class/Parant'
-
+import EvenDemo from './Day4/Day4Class'
+import Day4fun from './Day4/Day4fun'
+import Counter from './Day4/Counter'
+import ChangeNameOnClick from './Day4/CHangeNameOnClick'
+import BgColor from './Day4/BgColor'
+import EmployeeDetails from './Day4/Fun/EmployeeDetails'
+import ParentTwo from './Assign/StateAndPrps'
 
 
 function App() {
@@ -20,7 +26,9 @@ function App() {
 
   return (
     <>
-      <h1>SevenMentor</h1>
+      <h1>SevenMentors</h1>
+      {/*
+      
       <Day2 day="1"/>
       <DayFirst/>
       <StatesInClass/>
@@ -28,15 +36,29 @@ function App() {
       <ClassComp name="Kara"/>
       <Employee name="Jon" id="x53" salary="$12000.00"/>
       <Day2 day="2"/>
-      {/* <CounterComponent/> */}
       <StateDemo/>
       <MyLifeComponent/>
       <Day2 day="3"/>
       <Day3Demo/>
       <LifeCycle3/>
       <Parent/>
+      <EvenDemo/>
+      <BgColor/>
+   <ParentTwo name="bharat" id="12" salary="5LPA"/> 
+     <Counter/>
+     <ChangeNameOnClick/>
+   <ParentTwo name="nilesh" id="55" salary="12LPA"/>  
+       <EmployeeDetails name="Nimesh Gala" id="T20" salary="12LPA"/>
+     <EmployeeDetails name="Yuraj Singh" id="T11" salary="10LPA"/>
 
-      
+   
+   */}
+ 
+ <Day2 day="5"/>
+
+ 
+
+    
        
     </>
   )
